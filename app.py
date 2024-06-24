c562374006b282d907f5d246ad41e03ca2f83a4a
Braian Cruz <87029054+braian1983@users.noreply.github.com>
2024-06-24 09:56:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,43 @@ pagina_clientes = planilha_clientes['Sheet1']
 
 for linha in pagina_clientes.iter_rows(min_row=2,values_only=True):
    nome, valor, cpf, vencimento = linha
+   print(nome)
+   print(valor)
+   print(cpf)
+   print(vencimento)
 
 driver = webdriver.Chrome()
-driver.get('inserir o site de consulta da empresa')
+driver.get('')
 sleep(5)
 campo_pesquisa = driver.find_element(By.XPATH,"")
 sleep(1)
 campo_pesquisa.send_keys(cpf)
 sleep(1)
 
-botao_pesquisar = driver.find_element(By.XPATH,"")
+botao_pesquisar = driver.find_element(By.XPATH, "")
 sleep(1)
 botao_pesquisar.click()
+sleep(5)
+campo_pesquisa = driver.find_element(By.XPATH,"")
+sleep(1)
+campo_pesquisa.send_keys(cpf)
+sleep(1)
+
+botao_pesquisar = driver.find_element(By.XPATH, "")
+sleep(1)
+botao_pesquisar.click()
+
+status = driver.find_element(By.XPATH, "//span[@id='statusLabel']")
+if status.text == 'em dia':
+   data_pagamento = driver.find_element(By.XPATH, "//p[@id='paymentDate']")
+   metodo_pagamento = driver.find_element(By.XPATH, "//p[@id='paymentMethod']")
+   
+   pagina_fechamento.append([nome, valor, cpf, vencimento, 'em dia', 'xxx','xxx'])
+   
+else:
+   planilha_fechamento = openpyxl.load_workbook(' planilha_fechamento.xlsx')
+   pagina_fechamento = planilha_fechamento['Sheet1']
+   
+   pagina_fechamento.append([nome, valor, cpf, vencimento, 'pendente'])
+   
+
